c3416d6c4b7c73749794983ffaa1529048bbd3c4
Vikashhh18 <Vikash.sharma1761@gmail.com>
2025-04-16 14:41:10 +0530

    vite config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    allowedHosts: ['lms-vdemy.onrender.com', 'localhost', '127.0.0.1'],
+  },
 })
 
+// vite.config.js
+// export default {
+// };
 
 // export default defineConfig({
 //   plugins: [
